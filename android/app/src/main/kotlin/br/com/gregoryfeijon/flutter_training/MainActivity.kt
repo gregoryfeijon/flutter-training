fe874a65c7d990d619d5ae301e9e23f1db089c16
gregory-feijon <gregory.feijon@outlook.com>
2021-01-28 00:04:04 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/gregoryfeijon/flutter_training/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gregoryfeijon.flutter_training
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login.page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFF5F5F5),
+        padding: EdgeInsets.only(
+          top: 80,
+          left: 20,
+          right: 20,
+          bottom: 40,
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 450,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.black12,
+                    offset: new Offset(1, 2.0),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: ListView(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Welcome,",
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text("Sign in to continue"),
+                          ],
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text("Sign Up"),
+                        ),
+                      ],
+                    ),
+                    TextFormField(
+                      //autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
